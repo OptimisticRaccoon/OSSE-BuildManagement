ec6138f3e5aa7c8637ac88dde6d4fb6a736ac6e7
Marco Mayer <r4qtor@gmail.com>
2021-05-04 22:41:42 +0200

    Update App.java
    - Added comments for JavaDoc

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -6,6 +6,12 @@ package de.hfu;
  */
 public class App 
 {
+	/**
+	 * Converts console Input to uppercase and prints it out.
+	 * Method returns after output and exits the program.
+	 * 
+	 * @param args for future use
+	 */
     public static void main( String[] args )
     {
 		String input = System.console().readLine();

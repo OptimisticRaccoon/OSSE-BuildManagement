432eb53eef5af63eabb9d4f243fa1f8e38e29f7a
Marco Mayer <r4qtor@gmail.com>
2021-05-04 22:17:02 +0200

    Initial commit

---
+++ src/main/java/de/hfu/App.java
@@ -0,0 +1,14 @@
+package de.hfu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		String input = System.console().readLine();
+		System.out.println(input.toUpperCase());
+    }
+}
